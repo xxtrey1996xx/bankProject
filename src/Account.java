4762252f2e21f23bbc2aad2e9267bac34c82ecf6
xxtrey1996xx <gscarborough@missouriwestern.edu>
2017-11-14 21:01:02 -0600

    Added empty methods

--- src/Account.java
+++ src/Account.java
@@ -1,6 +1,34 @@
 public abstract class Account {
     String accountNumber;
-    String accountOwner;
+    String ownerName;
+    String ownerId;
     double balance;
 
+    public Account() {
+
+    }
+
+    public void openAccount() {
+
+    }
+
+    public void closeAccount() {
+
+    }
+
+    public void report() {
+
+    }
+
+    public void creditAccount() {
+
+    }
+
+    public void debitAccount() {
+
+    }
+
+    public void showLog() {
+
+    }
 }
