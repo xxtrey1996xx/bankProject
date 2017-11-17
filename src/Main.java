7efcbc91554926f89858d75c00c41629d2793739
xxtrey1996xx <gscarborough@missouriwestern.edu>
2017-11-16 20:54:13 -0600

    added arraylist

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,10 @@ import java.util.Scanner;
 
 public class Main {
 
+    static ArrayList<String> users = new ArrayList<>();//The Array for users
 
     public static void main(String[] args) {
-        ArrayList<Checking> checkings = new ArrayList<Checking>();
+
         readDB();
         firstMainMenu mainMenu = new firstMainMenu();
 
@@ -16,12 +17,18 @@ public class Main {
     }
 
     public static void readDB() {
-        File db = new File("bankdatabase.txt");
+        File db = new File("bankdatabase.txt");//connecting to the database
         try {
             Scanner input = new Scanner(db);//Open Scanner
             input.useDelimiter("\r");//Setting Delimeter to Tab
-            while (input.hasNext()) {
+            while (input.hasNext()) {//Checking if their is another record in the file
                 System.out.println(input.next());//TODO This is where we would seperate the account types into the different arrays.
+                String record = input.next();
+                String[] splitRecord = record.split("\t");//splitting the line by tabs
+                if (!users.contains(splitRecord[0])) {//Checking to see if the users group has that SSN in it already
+                    users.add(splitRecord[0]);//If not, then add it
+                    System.out.println("Found New User");
+                }
             }
             input.close();
         } catch (FileNotFoundException e) {
