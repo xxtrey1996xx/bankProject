5eee0378c3ea4a10448c0459fe84beedf37fdca1
xxtrey1996xx <gscarborough@missouriwestern.edu>
2017-11-27 19:41:51 -0600

    Reworking

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,56 @@
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Modality;
-import javafx.stage.Stage;
-import javafx.stage.StageStyle;
-import javafx.stage.Window;
-
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-    public static ArrayList<User> users = new ArrayList<>();
+    public static ArrayList<Customer> customers = new ArrayList<>();
 
     public static void main(String[] args) throws Exception {
-
+        readDB();
         LogInScreen lis = new LogInScreen();
         lis.pack();
         lis.setVisible(true);
     }
+
+    public static void readDB() throws Exception {
+        File db = new File("testDB.txt");
+        Scanner input = new Scanner(db);
+        input.nextLine(); //Skip title line.
+
+        input.useDelimiter("\n");//Set Delimiter to returns to get a record from the Database
+        while (input.hasNext()) {
+            String record = input.next();
+            System.out.println("This is the original DB Record: \n" + record);
+            String[] splitRecord = record.split("\\|");
+            updateUserArray(splitRecord);
+
+
+            //Scanner temp = new Scanner(String.valueOf(input));
+            //int count = 0;
+            //while (input.hasNext()) {
+            //    splitRecord[count] = input.next();
+            //    count++;
+        }
+        input.close();
+    }
+
+    public static void updateUserArray(String[] record) {
+        Customer obj;
+        obj = new Customer(record[0], record[5], record[6], record[1], record[2], record[3], record[4]);
+        customers.add(obj);
+        String accountType = record[8];
+        System.out.println("AccountType should be: " + accountType);
+        Double balance = Double.valueOf(Integer.parseInt(record[9]));
+        System.out.println("Balance should be: " + balance);
+        //Need to create account here.
+        //Account account = new Account();
+
+
+        //Validate that user has this account already.
+
+
+    }
+
 }
      /*   launch(args);
         readDB();
@@ -55,8 +85,8 @@ public class Main {
 
 
     public static void updateUserArray(String[] record) {
-        User obj;
-        obj = new User(record[0], record[5], record[6], record[1],record[2],record[3],record[4]);
+        Customer obj;
+        obj = new Customer(record[0], record[5], record[6], record[1],record[2],record[3],record[4]);
         users.add(obj);
         String accountType = record[8];
         System.out.println("AccountType should be: " + accountType);
