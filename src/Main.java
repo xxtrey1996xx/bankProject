841150807f97f7fddc34c7040cef8a3f208e9a76
xxtrey1996xx <gscarborough@missouriwestern.edu>
2017-11-30 21:18:42 -0600

    Updated updateCustomerArray to allow for all of the account types. Most of the functionality is disable by comments

--- src/Main.java
+++ src/Main.java
@@ -35,31 +35,128 @@ public class Main {
     }
 
     public static void updateCustomerArray(String[] record) {
-        //Assigns Parsed values to variables
-        String ssn = record[0];
-        String address = record[1];
-        String city = record[2];
-        String state = record[3];
-        String zip = record[4];
-        String fName = record[5];
-        String lName = record[6];
-        String acctNum = record[7];
-        String type = record[8];
-        String balance = record[9];
-        String interest = record[10];
-        String date = record[11];
+        if (record[8].compareToIgnoreCase("Savings") == 0) {
+            //Assigns Parsed values to variables
+            String ssn = record[0];
+            String address = record[1];
+            String city = record[2];
+            String state = record[3];
+            String zip = record[4];
+            String fName = record[5];
+            String lName = record[6];
+            String acctNum = record[7];
+            String type = record[8];
+            String balance = record[9];
+            String interest = record[10];
+            String date = record[11];
 
-        //Creates customer object from parsed values
-        Customer newCustomer;
-        newCustomer = new Customer(ssn, fName, lName, address, city, state, zip);
-        customers.add(newCustomer);
+            //Creates customer object from parsed values
+            Customer newCustomer;
+            newCustomer = new Customer(ssn, fName, lName, address, city, state, zip);
+            customers.add(newCustomer);
 
-        //Creates Account object and assigns to Customer objects Account Array.
-        //if(type == "Savings")
-        newCustomer.accounts.add(new Savings(ssn, balance, interest, acctNum, type, date));
-        System.out.println("Account: " + acctNum + "-" + type + " was added to user " + newCustomer.firstName + " " + newCustomer.lastName);
+            //Creates Account object and assigns to Customer objects Account Array
+            newCustomer.accounts.add(new Savings(ssn, balance, interest, acctNum, type, date));
+            System.out.println("Account: " + acctNum + "-" + type + " was added to user " + newCustomer.firstName + " " + newCustomer.lastName);
 
-        //Validate that user has this account already.
+            //Validate that user has this account already.
+        } else if (record[8].compareToIgnoreCase("CD") == 0) {
+            //Assigns Parsed values to variables
+            String ssn = record[0];
+            String address = record[1];
+            String city = record[2];
+            String state = record[3];
+            String zip = record[4];
+            String fName = record[5];
+            String lName = record[6];
+            String acctNum = record[7];
+            String type = record[8];
+            String balance = record[9];
+            String interest = record[10];
+            String date = record[11];
+
+            //TODO This is commented out until we are ready for it.
+            //Creates customer object from parsed values
+            /*
+            Customer newCustomer;
+            newCustomer = new Customer(ssn, fName, lName, address, city, state, zip);
+            customers.add(newCustomer);
+
+            //Creates Account object and assigns to Customer objects Account Array.
+            newCustomer.accounts.add(new Savings(ssn, balance, interest, acctNum, type, date));
+            System.out.println("Account: " + acctNum + "-" + type + " was added to user " + newCustomer.firstName + " " + newCustomer.lastName);
+            */
+
+            //Validate that user has this account already.
+        } else if (record[8].compareToIgnoreCase("TMB") == 0 || record[8].compareToIgnoreCase("Gold") == 0 || record[8].compareToIgnoreCase("Diamond") == 0) {
+            //Assigns Parsed values to variables
+            String ssn = record[0];
+            String address = record[1];
+            String city = record[2];
+            String state = record[3];
+            String zip = record[4];
+            String fName = record[5];
+            String lName = record[6];
+            String acctNum = record[7];
+            String type = record[8];
+            String bal = record[9];
+            String wtfIsThis = record[10];//TODO what is this part of the record?
+            String date = record[11];
+            String hasOverdraftAccount = record[12];
+            boolean hasOverdraftAccountBool;
+            //TODO We need to setup the overdraft account link
+
+            /*
+            Customer newCustomer;
+            newCustomer = new Customer(ssn, fName, lName, address, city, state, zip);
+            customers.add(newCustomer);
+
+            //Creates Account object and assigns to Customer objects Account Array.
+            if(hasOverdraftAccount = 1)//This is setting the boolean value from the string
+                hasOverDraftAccountBool = true;
+            else
+                hasOverdraftAccountBool = false;
+            newCustomer.accounts.add(new Checking(ssn, balance, interest, acctNum, type, date,hasOverDraftAccountBool,null));
+            System.out.println("Account: " + acctNum + "-" + type + " was added to user " + newCustomer.firstName + " " + newCustomer.lastName);
+            */
+        } else if (record[8].compareToIgnoreCase("CC") == 0) {
+            //Assigns Parsed values to variables
+            String ssn = record[0];
+            String address = record[1];
+            String city = record[2];
+            String state = record[3];
+            String zip = record[4];
+            String fName = record[5];
+            String lName = record[6];
+            String cardNum = record[7];
+            String type = record[8];
+            String bal = record[9];
+            String intRate = record[10];
+            String mostRecentPayDate = record[11];//TODO IS that what this date is?
+            String openedDate = record[12];
+            String monthlyPayment = record[13];
+            String dueDate = record[14];
+            String unknownVariable = record[15];//TODO What is this variable?
+        } else if (record[8].compareToIgnoreCase("short") == 0 || record[8].compareToIgnoreCase("long") == 0) {
+            String ssn = record[0];
+            String address = record[1];
+            String city = record[2];
+            String state = record[3];
+            String zip = record[4];
+            String fName = record[5];
+            String lastName = record[6];
+            String acctNum = record[7];
+            String type = record[8];
+            String balance = record[9];
+            String interestRate = record[10];
+            String mostRecentPayDate = record[11];
+            String openedDate = record[12];
+            String monthlyPayment = record[13];
+            String dueDate = record[14];
+            String unknownVariable = record[15];
+        } else {
+            System.out.println("Found Invalid Account Type! " + record[0] + " " + record[8]);
+        }
 
 
     }
